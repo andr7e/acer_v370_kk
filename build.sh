cf271b5534946004a10794e7fc7f231c34c529c4
andr7e <andr13e@gmail.com>
2015-05-29 20:55:08 +0300

    add multi config support; available acer89_v370_wet_kk, fly89_iq446_wet_kk, philips89_w6500_wet_kk

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#script compile kernel acer e2 by doha saizo
+#script compile kernel by andr7e
 #setting build
 
 toolchain="$HOME/kernel_build/android-toolchain-eabi-4.9/bin/arm-linux-androideabi-"
@@ -13,23 +13,29 @@ export USE_CCACHE=1
 export CROSS_COMPILE=$toolchain
 
 mtktools_path="$source_path/mtktools"
-dest_path="$mtktools_path/doha"
 dest_cwm_path="$mtktools_path/cwm_recovery"
 dest_twrp_path="$mtktools_path/twrp_recovery"
 
 build_kernel()
-{
-   ./makeMtk -o=TARGET_BUILD_VARIANT=user n k
+{ 
+   projectName="$1";
 
-   cd $source_path/mediatek/build/tools
-   ./mkimage $source_path/out/target/product/doha86_wet_kk/obj/KERNEL_OBJ/arch/arm/boot/zImage KERNEL > $source_path/mtktools/boot.img-kernel.img
+   echo "$projectName";
+  
+   ./makeMtk -o=TARGET_BUILD_VARIANT=user -t "$projectName" r k
 
-   cd $source_path/mtktools
-   ./repack.pl -boot boot.img-kernel.img boot.img-ramdisk $source_path/mtktools/doha/boot.img
+   projectKernelName="boot.img-kernel-$projectName.img"
+   projectDataName="$mtktools_path/$projectName"
 
-   cd $source_path/mtktools/doha
+   cd "$source_path/mediatek/build/tools"
+   ./mkimage $source_path/out/target/product/"$projectName"/obj/KERNEL_OBJ/arch/arm/boot/zImage KERNEL > "$mtktools_path/$projectKernelName"
+
+   cd "$mtktools_path"
+   ./repack.pl -boot "$projectKernelName" boot.img-ramdisk "$projectDataName/boot.img"
+
+   cd "$projectDataName"
    zip -r out .
-   mv $source_path/mtktools/doha/out.zip $source_path/kernel3.4.67KK.zip
+   mv "$projectDataName/out.zip" "$source_path/$projectName-kernel3.4.67KK.zip"
 }
 
 repack_recovery()
@@ -52,9 +58,21 @@ repack_recovery()
 
 recovery_param="recovery"
 
-if [ "$1" = "$recovery_param" ];
+
+: 'if [ "$1" = "$recovery_param" ];
 then
    repack_recovery
 else
    build_kernel
+fi'
+
+
+if [ "$1" = "list" ];
+then
+   echo "Available projects:"
+   echo "acer89_v370_wet_kk"
+   echo "fly89_iq446_wet_kk"
+   echo "philips89_w6500_wet_kk"
+else
+   build_kernel "$1"
 fi
